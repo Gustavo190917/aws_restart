b758e657213040db30fab96d0c3f852a01e219fb
Gustavo190917 <105766688+Gustavo190917@users.noreply.github.com>
2022-05-18 19:35:27 -0500

    Add files via upload

---
+++ Programas.py
@@ -0,0 +1,5 @@
+numaterias = input("Que numero de materias tiene? ")
+for x in numaterias :
+    materia = input("Que materia tiene? ")
+    calificacion = input("Cual es su calificacion? ")
+    print("Yo estudio {} y saque {}".format(materia,calificacion))
---
+++ collections.py
@@ -0,0 +1,23 @@
+myFruitList = ["apple", "banana", "cherry"]#Este se puede modificar
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "orange" #Cambio de informacion
+print(myFruitList)
+myFinalAnswerTuple = ("apple", "banana", "pineapple") #Este ya no se puede modificar
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
